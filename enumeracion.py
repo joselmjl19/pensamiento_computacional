d840965cf3bf704fd849c81b9f378b7d31507d9e
Jose Luis <joselm.jl19@gmail.com>
2021-01-05 20:43:04 -0600

    el archivo iteraciones.py contiene basicamente el ciclo while y el archivo enumaracion.py es el algoritmo de enumeracion exhaustiva

---
+++ enumeracion.py
@@ -0,0 +1,18 @@
+#algoritmo de enumeracion exhaustiva
+
+def run():
+    objetivo = int(input('Escribe un entero: '))
+    respuesta = 0
+
+    while respuesta ** 2 < objetivo:
+        respuesta += 1
+
+    if respuesta ** 2 == objetivo:
+        print(f'La raiz cuadrada de {objetivo} es {respuesta}')
+    else:
+        print(f'{objetivo} no tiene una raiz cuadrada exacta')
+
+
+
+if __name__ == '__main__':
+    run()
